49dba0b77abfdd24026f5b4d0afd86dab94aad75
Tri Leo <trileomg@gmail.com>
2024-03-17 09:33:56 +0700

    new

--- tuan2/firebaseconfig.js
+++ tuan2/firebaseconfig.js
@@ -1,29 +1,52 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-
-import { getAuth, createUserWithEmailAndPassword, } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js'
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 const firebaseConfig = {
-    apiKey: "AIzaSyDDFYrCYbp5nU-eyghyrSGjhwG6p6V8-WI",
-    authDomain: "jsi04-ceaad.firebaseapp.com",
-    projectId: "jsi04-ceaad",
-    storageBucket: "jsi04-ceaad.appspot.com",
-    messagingSenderId: "764041360842",
-    appId: "1:764041360842:web:56e615f66e2e403abf0b33",
-    measurementId: "G-8K81HF4ZZS"
+    apiKey: "AIzaSyCGPMHYXP26MUOxsl5sJTdwR2gaIymJ6q4",
+    authDomain: "tuan2jsi-52524.firebaseapp.com",
+    projectId: "tuan2jsi-52524",
+    storageBucket: "tuan2jsi-52524.appspot.com",
+    messagingSenderId: "710484892198",
+    appId: "1:710484892198:web:6875d39636fb2be0bde4e7",
+    measurementId: "G-VELJM3ZK3V",
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth()
-export const dangky = async (auth, email, password) => {
+export const auth = getAuth();
+
+export const dangky = async (auth, email, pass) => {
     let isSuccess;
     let infoMessage;
     try {
-        const user = await createUserWithEmailAndPassword(auth, email, password);
-        isSuccess = true
+        const user = await createUserWithEmailAndPassword(auth, email, pass);
+        isSuccess = true;
     } catch (error) {
-
-        isSuccess = false
-        infoMessage = err.code
+        isSuccess = false;
+        infoMessage = error.code;
     }
-    return { isSuccess, infoMessage }
-}
-export const dangnhap = async()
+
+    return {
+        isSuccess,
+        infoMessage,
+    };
+};
+
+export const dangnhap = async (auth, email, pass) => {
+    let isSuccess;
+    let infoMessage;
+    try {
+        const user = await signInWithEmailAndPassword(auth, email, pass);
+        isSuccess = true;
+    } catch (error) {
+        isSuccess = false;
+        infoMessage = error.code;
+    }
+    return {
+        isSuccess,
+        infoMessage,
+    };
+};
--- tuan2/index.js
+++ tuan2/index.js
@@ -1,10 +1,20 @@
-import { auth, dangky } from "./firebaseConfig.js";
-console.log(auth)
-const loginBtn = document.querySelector("#btnlogin");
-loginBtn.addEventListener("click", () => {
+import { auth, dangky, dangnhap } from "./firebaseConfig.js";
+
+const loginBtn = document.querySelector("#btnLogin");
+
+loginBtn.addEventListener("click", async function () {
     let email = document.querySelector("#form2Example17");
     let pass = document.querySelector("#form2Example27");
-    const message = dangky(auth, email.value, pass.value);
+    const { isSuccess, infoMessage } = await dangky(
+        auth,
+        email.value,
+        pass.value
+    );
+    if (isSuccess) {
+        alert("Đăng ký thành công");
+    } else {
+        if (infoMessage == "auth/missing-password") {
+            pass.classList.add("errorPass");
+        }
+    }
 });
-
-
